28bd4026128f458ea14812fbb00dfc9db06e0026
Markus Wedler <markuswedler8@gmail.com>
2024-01-07 14:43:22 +1000

    SCSS modules

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import classes from "./App.module.scss"
+
+export default () => {
+	const [data, setData] = useState(0)
+	return (
+		<div className={classes.container}>
+			<button onClick={() => setData((prev) => prev - 1)} className={classes.button}>
+				-
+			</button>
+			<div className={classes.value}>{data}</div>
+			<button onClick={() => setData((prev) => prev + 1)} className={classes.button}>
+				+
+			</button>
+		</div>
+	)
+}
